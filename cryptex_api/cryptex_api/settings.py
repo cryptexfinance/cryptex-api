743efaa8e8053edf38307320eae7e6c1dcb2ceb5
jdestephen <jdestephen07@gmail.com>
2023-02-02 17:36:50 -0600

    nginx prod conf

--- cryptex_api/cryptex_api/settings.py
+++ cryptex_api/cryptex_api/settings.py
@@ -149,15 +149,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(Path(BASE_DIR).parent, 'static/')
 
 # Base url to serve media files
 MEDIA_URL = '/images/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+MEDIA_ROOT = os.path.join(Path(BASE_DIR).parent, 'images/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
