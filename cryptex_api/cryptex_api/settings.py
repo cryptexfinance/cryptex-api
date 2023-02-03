1788748c43b8fc6385b8ebcb8529cf538072ffa1
jdestephen <jdestephen07@gmail.com>
2023-02-02 18:01:46 -0600

    added trusted origins

--- cryptex_api/cryptex_api/settings.py
+++ cryptex_api/cryptex_api/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://api.cryptex.finance",]
+CSRF_TRUSTED_ORIGINS = ["https://api.cryptex.finance", "https://test-api.cryptex.finance",]
 
 # Application definition
 
