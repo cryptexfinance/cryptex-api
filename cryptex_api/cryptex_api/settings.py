cb6a878ba829858fc52e841edfea1cabedf6ec52
jdestephen <jdestephen07@gmail.com>
2023-02-04 11:28:56 -0600

    generatenew secret key

--- cryptex_api/cryptex_api/settings.py
+++ cryptex_api/cryptex_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u9ip*+n76zj0v3%0u#g(s7s93^vgn#(9eu31!0#6^f=22s=715'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
